1238acb829be788f2d92b001b0e88dbdbca42784
Rahmatxonov <rahmatxonov1001@gmail.com>
2023-12-26 10:36:58 +0500

    add header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,22 @@
-// import { useState, useEffect } from "react";
-// import { HiOutlineSearch } from "react-icons/hi";
-// import { SlMenu } from "react-icons/sl";
+import { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
 // import { VsChromeClose } from "react-icons/vsc";
-// import { useNavigate, useLocation } from "react-router-dom";
-// import ContentWrapper from "../contentWrapper/ContentWrapper";
-// import logo from "../../assets/img/movix-logo.png";
-// import "./header.scss";
+import { useNavigate, useLocation } from "react-router-dom";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/img/movix-logo.png";
+import "./header.scss";
 
-// const Header = () => {
-//   const [show, setShow] = useState("top");
-//   const [lastScrollY, setLastScrollY] = useState(0);
-//   const [mobileMenu, setMobileMenu] = useState(false);
-//   const [query, setQuery] = useState("");
-//   const [showSearch, setShowSearch] = useState("");
-//   const navigate = useNavigate();
-//   const location = useLocation();
+const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-//   return <div>Header</div>;
-// };
+  return <div>Header</div>;
+};
 
-// export default Header;
+export default Header;
